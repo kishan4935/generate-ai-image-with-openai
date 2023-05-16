a963d82565ae7b1b232cbd62fdd21c7d2a88927d
Kishan Kumar <kishan1601kumar@gmail.com>
2023-05-17 00:04:27 +0530

    api fetch add

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,60 @@ import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [placeholder, setPlaceholder] = useState(
+    "Search Bears with Paint Brushes the Starry Night, painted by Vincent Van Gogh.."
+  );
   const configuration = new Configuration({
-    apikey: import.meta.env.VITE_Open_AI_Key,
+    apiKey: import.meta.env.VITE_Open_AI_Key,
   });
 
   const openai = new OpenAIApi(configuration);
 
   const generateImage = async () => {
+    setPlaceholder(`Search ${prompt}..`);
+    setLoading(true);
     const res = await openai.createImage({
-      promt: "say this is a test",
+      prompt: prompt,
       n: 1,
-      size: "1024x1024",
+      size: "512x512",
     });
-    console.log(res.data.data[0].url);
+    setLoading(false);
+    setResult(res.data.data[0].url);
   };
+  return (
+    <div className="app-main">
+      {loading ? (
+        <>
+          <h2>Generating..Please Wait..</h2>
+          <div class="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <>
+          <h2>Generate an Image using Open AI API</h2>
 
-  return (<div>
-    <button onclick={generateImage}>Generate an Image</button>
-  </div>);
-  
+          <textarea
+            className="app-input"
+            placeholder={placeholder}
+            onChange={(e) => setPrompt(e.target.value)}
+            rows="10"
+            cols="40"
+          />
+          <button onClick={generateImage}>Generate an Image</button>
+          {result.length > 0 ? (
+            <img className="result-image" src={result} alt="result" />
+          ) : (
+            <></>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
