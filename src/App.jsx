fd18766537465a44e73f77f4636a82b0c6b07009
Kishan Kumar <kishan1601kumar@gmail.com>
2023-05-16 23:09:57 +0530

    Button add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-
-import './App.css'
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
 
 function App() {
+  const configuration = new Configuration({
+    apikey: import.meta.env.VITE_Open_AI_Key,
+  });
 
+  const openai = new OpenAIApi(configuration);
 
-  return (
-    <>
-      Hello World 
-    </>
-  )
+  const generateImage = async () => {
+    const res = await openai.createImage({
+      promt: "say this is a test",
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log(res.data.data[0].url);
+  };
+
+  return (<div>
+    <button onclick={generateImage}>Generate an Image</button>
+  </div>);
+  
 }
 
-export default App
+export default App;
